aa2341719988954f85d11be6afee747a1733d0ea
Tay <b1233967@gmail.com>
2024-05-02 00:06:04 +0800

    Create routes and controller for multiple apis

---
+++ devcamper_api/routes/bootcamps.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require("../controllers/bootcamps");
+
+router.route("/").get(getBootcamps).post(createBootcamp);
+
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+// Without controller
+// router.get("/", (req, res) => {
+//   res.status(200).json({ success: true, msg: "Show all bootcamps" });
+// });
+
+// router.get("/:id", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+// });
+
+// router.post("/", (req, res) => {
+//   res.status(200).json({ success: true, msg: "Create all bootcamps" });
+// });
+
+// router.put("/:id", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+// });
+
+// router.delete("/:id", (req, res) => {
+//   res
+//     .status(200)
+//     .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+// });
+
+module.exports = router;
