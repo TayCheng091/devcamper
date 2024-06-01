7e5ec0ac7d720045b12240633a398cedb0a11f5f
Tay <b1233967@gmail.com>
2024-06-01 23:25:34 +0800

    register api & encrypt password with bcryptjs

---
+++ devcamper_api/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please add a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please add an email"],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please add a valid email",
+    ],
+  },
+  role: {
+    type: String,
+    enum: ["user", "publisher"],
+    default: "user",
+  },
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false, // Don't return the password when querying the database
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Encrypt password using bcrypt
+UserSchema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
